51cffe7028f9be15ef9a60cb859c90e87855d271
daeheon <eogjs99@gmail.com>
2021-06-05 03:30:33 +0900

    stash lerna 가 작동안해서 봐야됨

---
+++ packages/backend/src/shared/base/api.ts
@@ -0,0 +1,11 @@
+import { Protocol } from '@nrh/protocols';
+
+// TODO: Req 감싸서 뭔가 덕지덕지 붙여야 함.
+// 이거 생각해보니 backend로 옮겨야...
+type HandlerFunction<T> = T extends Protocol<infer Req, infer Resp, any>
+	? (body: Req) => Promise<Resp>
+	: never;
+
+export type Api<T> = {
+	[P in keyof T]: HandlerFunction<T[P]>;
+}
---
+++ packages/protocols/src/common/protocol.ts
@@ -0,0 +1,22 @@
+import yup from 'yup';
+import { TypedSchema } from 'yup/lib/util/types';
+
+export type HttpMethod = 'get' | 'post' | 'delete' | 'put';
+
+export enum DatabaseConnectionMode {
+	NO_DB = 'no_db',
+	ATOMIC_REQUEST = 'atomic_request',
+}
+
+export interface Protocol<Req, Resp, Schema extends TypedSchema> {
+	method: HttpMethod;
+	resource: string;
+	page: string;
+	schema: yup.InferType<Schema> extends Req ? Schema : 'error: wrong schema';
+
+	tmp_req?: Req;
+	tmp_resp?: Resp;
+}
+
+export type ProtocolReqType<T> = T extends Protocol<infer Req, infer Resp, any> ? Req : never;
+export type ProtocolRespType<T> = T extends Protocol<infer Req, infer Resp, any> ? Resp : never;
