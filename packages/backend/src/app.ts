0ff4fcd73ceaa89057e565e544483dac08a27530
daeheon <eogjs99@gmail.com>
2021-06-14 16:07:45 +0900

    대충 돌아가는 무언가

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import { sampleRouter } from './sample';
 
 const app = express();
 const router = express.Router();
+
+router.use('/', sampleRouter);
 
 app.use(router);
 
