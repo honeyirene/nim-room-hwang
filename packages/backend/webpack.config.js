32e71288119e105af740eabaa1550421e63f26b3
daeheon <eogjs99@gmail.com>
2021-06-18 17:35:54 +0900

    webpack config 갱신

--- packages/backend/webpack.config.js
+++ packages/backend/webpack.config.js
@@ -1,19 +1,52 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
+const ForkTsCheckerNotifierWebpackPlugin = require('fork-ts-checker-notifier-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const NodemonPlugin = require('nodemon-webpack-plugin');
+const webpack = require('webpack');
 
 const env = process.env.NODE_ENV || 'production';
+const isLocal = !!process.env.isLocal;
+
 const rootPath = path.resolve(__dirname);
 const srcPath = path.resolve(rootPath, 'src');
 
+function makePlugin() {
+	const plugins = [
+		new webpack.ProgressPlugin(),
+	];
+
+	if (env === 'development') {
+		const plugins_development = [
+			new ForkTsCheckerWebpackPlugin({
+				tslint: false,
+				useTypescriptIncrementalApi: true,
+			}),
+			new ForkTsCheckerNotifierWebpackPlugin({
+				title: 'TypeScript',
+				excludeWarnings: false,
+			}),
+		]
+		for (const p of plugins_development) { plugins.push(p); }
+	}
+
+	if (isLocal) {
+		plugins.push(new NodemonPlugin());
+	}
+
+	return plugins;
+}
+
 const config = {
+	mode: env,
 	entry: path.resolve(srcPath, 'index.ts'),
 	target: 'node',
+	devtool: 'source-map',
 	output: {
 		libraryTarget: 'commonjs',
 		path: path.join(__dirname, 'dist'),
 		filename: '[name].js',
 	},
-	devtool: 'source-map',
 	module: {
 		rules: [
 			// all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
@@ -29,10 +62,10 @@ const config = {
 			},
 		],
 	},
-	mode: env,
 	resolve: {
 		extensions: [".ts", ".js", ".json"],
 	},
+	plugins: makePlugins(),
 	externals: [nodeExternals({
 		additionalModuleDirs: ['../../node_modules'],
 		allowlist: ['@nrh/protocols'],
