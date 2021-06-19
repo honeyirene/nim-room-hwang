03d356d3f52feaf0f1f9786482e01f3ce4d44af6
daeheon <eogjs99@gmail.com>
2021-06-20 00:48:55 +0900

    네트워크가 들어간 예시

---
+++ packages/frontend/src/components/sample/SampleComponent.tsx
@@ -0,0 +1,60 @@
+import React, { useMemo, useState } from 'react';
+import { Button } from 'react-native';
+import { ClientGroup } from '../../shared';
+
+interface Props {
+	name: string
+}
+
+export const SampleComponent: React.FC<Props> = props => {
+	const { name } = props;
+
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+
+	const [a, setA] = useState<string>('sample');
+	const [b, setB] = useState<number>(0);
+
+	const client = useMemo(() => {
+		const group = new ClientGroup();
+		return group.sample;
+	}, []);
+
+	const handleA = (evt: React.ChangeEvent<HTMLInputElement>) => {
+		const v = evt.target.value;
+		setA(v);
+	}
+
+	const handleB = (evt: React.ChangeEvent<HTMLInputElement>) => {
+		const v = evt.target.valueAsNumber;
+		setB(v);
+	}
+
+	const onClick = async () => {
+		setIsLoading(true);
+		await client.sample({ a, b });
+		setIsLoading(false);
+	}
+
+	if (isLoading) {
+		return <h1>loading...</h1>;
+	}
+
+	return (
+		<>
+			<h1>{name} counter</h1>
+			<input
+				type="string"
+				onChange={handleA}
+			/>
+			<input
+				type="number"
+				onChange={handleB}
+			/>
+
+			<Button
+				onPress={onClick}
+				title={'send'}
+			/>
+		</>
+	);
+}
