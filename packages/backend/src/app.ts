3ee2ca3c74a1d7b82189bf90feb500833819910b
daeheon <eogjs99@gmail.com>
2021-06-22 16:32:30 +0900

    add cors

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { resource_sample } from '@nrh/protocols';
 import { sampleRouter } from './sample';
 
 const app = express();
+
 const router = express.Router();
+router.use(cors());
 
 router.use(resource_sample, sampleRouter);
 
