7ffab105c60b8a249bf09a03cb5d35b8cac61dd2
daeheon <eogjs99@gmail.com>
2021-07-02 21:27:07 +0900

    [WIP] Feature/front network (#3)
    * 대충 client 구현
    
    * 네트워크가 들어간 예시
    
    * 왜 터짐?
    
    * stash
    
    * add cross-env
    
    * stash
    
    * fix
    
    * 코드를 더 엄격하게
    
    * cra 버전 낮춤
    
    * fix up code
    
    * fix error
    
    * add cors and fix package.json
    
    * @types 는 dev dependency로
    
    * add cors
    
    * 요청을 json으로 받아야함

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import { resource_sample } from '@nrh/protocols';
 import { sampleRouter } from './sample';
 
 const app = express();
+
 const router = express.Router();
+router.use(express.json({ limit: '5MB' }));
+router.use(express.urlencoded({ extended: true }));
+router.use(cors());
 
 router.use(resource_sample, sampleRouter);
 
--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,11 +1,13 @@
-import * as React from "react"
-import * as ReactDOM from "react-dom"
+import React from "react"
+import ReactDOM from "react-dom"
 import Counter from "./components/Counter"
+import { SampleComponent } from "./components/sample";
 
 // index.tsx
-ReactDOM.render( 
-  <React.StrictMode>
-    <Counter name='react'/> 
-  </React.StrictMode>,  
-  document.getElementById("root")
+ReactDOM.render(
+	<React.StrictMode>
+		<Counter name='react' />
+		<SampleComponent name='nim room hwang' />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
